7f45e65c0ad8ce7620a79aeece79d7890f85bb35
rohini <patilrohini43@gmail.com>
2019-12-16 11:09:42 +0530

    Added:using ramdom to find head and tail

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -2,4 +2,16 @@
 
 echo "Welcome to FlipCoin"
 
+head=0
+tail=0
 
+random=$(( RANDOM % 2))
+if [[ $random -eq 1 ]]
+then
+	head=$(( head + 1 ))
+else
+	tail=$(( tail + 1 ))
+fi
+
+echo $head
+echo $tail
