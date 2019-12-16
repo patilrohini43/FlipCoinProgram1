631332372b9fb0228fbdd054e1e9337784b8d864
rohini <patilrohini43@gmail.com>
2019-12-16 11:58:04 +0530

    Added:finding doublet of head and tail and calculate percentage

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -6,7 +6,7 @@ echo "Welcome to FlipCoin"
 read -p "Enter number of the time you want to flip a coin" flipNumber
 
 declare -A flipCoin
-coinNum=1
+coinNum=2
 H=0
 T=0
 for(( i=0;i<$flipNumber;i++))
